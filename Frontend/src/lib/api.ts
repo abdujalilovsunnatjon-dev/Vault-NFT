64f6440fa00d76bf4e7e3cae66a885754460ce7b
abdujalilovsunnatjon-dev <abdujalilovsunnatjon.dev@gmail.com>
2026-01-12 16:35:41 +0500

    api.ts

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,7 +1,6 @@
-// Frontend/src/lib/api.ts
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // <- должен быть установлен в Vercel
-  withCredentials: true, // если бекенд использует куки/сессии
+  baseURL: "https://vault-nft-backend.onrender.com",
+  withCredentials: true,
 });
